38b48ce37e0af8259e60ecf8c16a93e313ccc256
Paolo G. Giarrusso <paolo@skylabs-ai.com>
2026-02-14 21:22:33 +0100

    prelude.stdpp_ssreflect: reuse more

--- rocq-skylabs-prelude/theories/stdpp_ssreflect.v
+++ rocq-skylabs-prelude/theories/stdpp_ssreflect.v
@@ -12,14 +12,8 @@
  * https://gitlab.mpi-sws.org/iris/iris/-/blob/26ebf1eed7d99a02683996e1b06c5f28870bf0a0/LICENSE-CODE
  *)
 
-(* Load both ssreflect and stdpp, using the same settings as Iris. *)
-Require Export Stdlib.ssr.ssreflect.
-Require Export stdpp.prelude.
-#[global] Open Scope general_if_scope.
-#[global] Set SsrOldRewriteGoalsOrder. (* See Coq issue #5706 *)
-Ltac done := stdpp.tactics.done.
+(* Load both ssreflect and stdpp, using the same settings as Iris in [vendored/rocq-iris/iris/prelude/prelude.v]. *)
+Require Export stdpp.ssreflect.
 
 (** Iris itself and many dependencies still rely on this coercion. *)
 Coercion Z.of_nat : nat >-> Z.
-
-#[global] Hint Mode Equiv ! : typeclass_instances.
