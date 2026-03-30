686a3761342c7f368ececfe0b154c8f17e6b27ce
Simon Hudon <simon@bluerock.io>
2026-03-30 17:09:37 -0400

    ltac2 builder: fix comment

--- ltac2-extra/theories/internal/builder.v
+++ ltac2-extra/theories/internal/builder.v
@@ -223,9 +223,9 @@ Module Builder.
       pair of lists of integers and return their concatenation as a Galina term:
       <<
         let build_int_list := build_list build_Z in
-        _apply '(@List.app) [Wildcard]
-          (_arg_on fst build_int_list)
-          (_arg_on snd build_int_list)
+        Ap.apply '(@List.app) [Wildcard]
+          (Ap.arg_on fst build_int_list)
+          (Ap.arg_on snd build_int_list)
           _done
       >>
 
