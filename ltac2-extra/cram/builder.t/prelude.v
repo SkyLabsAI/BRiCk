91877c51cdf17ebacf75f8cba5206d22d37f765a
Simon Hudon <simon@bluerock.io>
2026-03-31 15:21:26 -0400

    ltac2 builder: move test case to cram test

---
+++ ltac2-extra/cram/builder.t/prelude.v
@@ -0,0 +1,30 @@
+(**
+ * Copyright (C) 2026 SkyLabs AI, Inc.
+ * This file is CONFIDENTIAL AND PROPRIETARY to SkyLabs AI. All rights reserved.
+ *
+ * Use of this file is only permitted subject to a separate written license agreement with SkyLabs AI.
+ *)
+Require Import skylabs.ltac2.extra.extra.
+
+Import Ltac2 Builder Printf.
+
+Open Scope list_scope.
+Import Lists.List.ListNotations.
+
+Ltac2 custom_list_builder (build_a : 'a Builder.t) (build_b : 'b Builder.t) : ('a * 'a * 'b) Builder.t :=
+  fun () =>
+  error_context! [fprintf "custom list builder"]
+  Ap.apply '(fun a b c => ([a] ++ [b] ++ List.rev c)%list) []
+     (Ap.arg_on (fun (a,_,_) => a) build_a)
+     (Ap.arg_on (fun (_,b,_) => b) build_a)
+     (Ap.arg_on (fun (_,_,c) => c) build_b)
+     Ap.done.
+
+Ltac2 faulty_list_builder (build_a : 'a Builder.t) (build_b : 'b Builder.t) : ('a * 'a * 'b) Builder.t :=
+  fun () =>
+  error_context! [fprintf "faulty list builder"]
+  Ap.apply '(fun a b c => (a ++ [b] ++ List.rev c)%list) []
+     (Ap.arg_on (fun (a,_,_) => a) build_a)
+     (Ap.arg_on (fun (_,b,_) => b) build_a)
+     (Ap.arg_on (fun (_,_,c) => c) build_b)
+     Ap.done.
---
+++ ltac2-extra/cram/builder.t/test2.v
@@ -0,0 +1,29 @@
+(**
+ * Copyright (C) 2026 SkyLabs AI, Inc.
+ * This file is CONFIDENTIAL AND PROPRIETARY to SkyLabs AI. All rights reserved.
+ *
+ * Use of this file is only permitted subject to a separate written license agreement with SkyLabs AI.
+ *)
+Require Import skylabs.ltac2.extra.extra.
+Require Import skylabs_auto.cram.builder.prelude.
+
+Section example.
+  Import Ltac2 Builder Printf.
+
+  Open Scope list_scope.
+  Import Lists.List.ListNotations.
+
+  Goal True.
+    printf "".
+    printf "(** Test 3 - error in arguments *)".
+    Ltac2 Eval
+      let builder :=
+        let builder_a := unsafe_constr '(nat) in
+        let builder_b := build_list build_nat in
+        custom_list_builder builder_a builder_b in
+
+      let trm     := run builder ( '([1]), '(5), [2;3;4]) in
+      Control.assert_true (Constr.equal trm '([1;5;4;3;2])).
+  Abort.
+
+End example.
