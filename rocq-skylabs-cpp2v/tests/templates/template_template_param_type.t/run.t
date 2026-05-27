deb42a8fefe1f00f64d530bdf25c1f65f4494e9f
Gregory Malecha <gregory@skylabs-ai.com>
2026-05-27 13:03:59 -0400

    Test case for template-template parameters (which are causing loops)

---
+++ rocq-skylabs-cpp2v/tests/templates/template_template_param_type.t/run.t
@@ -0,0 +1,2 @@
+  $ . ../../setup-cpp2v.sh
+  $ cpp2v -o /dev/null --templates /dev/null test.cpp -- -std=c++23
---
+++ rocq-skylabs-cpp2v/tests/templates/template_template_param_type.t/test.cpp
@@ -0,0 +1,5 @@
+template <template <class> class L>
+struct B;
+
+template <template <class> class L>
+void f(B<L>);
