fc91e7c62d44ff2692b47cfd5443d99bffe5bd11
Gregory Malecha <gregory@skylabs-ai.com>
2026-06-04 11:45:21 -0700

    Test cases for dependent names

--- rocq-skylabs-cpp2v/src/PrintExpr.cpp
+++ rocq-skylabs-cpp2v/src/PrintExpr.cpp
@@ -20,6 +20,7 @@
 #include "clang/Basic/Builtins.h"
 #include "clang/Basic/Version.inc"
 #include "llvm/ADT/APFloat.h"
+#include <clang/AST/OperationKinds.h>
 #include <optional>
 
 using namespace clang;
@@ -189,15 +190,47 @@ struct PrintDependentName : public ConstStmtVisitor<PrintDependentName, void> {
     }
 
     void VisitUnresolvedMemberExpr(const UnresolvedMemberExpr *expr) {
-        auto name = expr->getName();
-        llvm::errs() << "printDeclarationName(" << expr->getName().getNameKind()
-                     << ")";
-        name.dump();
-
-        print.output() << "(Nunsupported \"VisitUnresolvedMemberExpr\")";
+        cprint.printUnresolvedName(print, expr->getQualifier(), expr->getName(),
+                                   expr->template_arguments(), loc::of(expr));
     }
+
+    void VisitMemberExpr(const MemberExpr *expr) {
+        guard::ctor _{print, "Ndependent"};
+        guard::ctor __{print, "Tresult_member"};
+        cprint.printQualType(print, expr->getBase()->getType(), loc::of(expr))
+            << fmt::nbsp;
+        cprint.printUnresolvedName(print, expr->getQualifier(),
+                                   expr->getMemberDecl()->getDeclName(),
+                                   loc::of(expr));
+    }
+
+    void VisitParenExpr(const ParenExpr *expr) {
+        Visit(expr->getSubExpr());
+    }
+
+    void VisitImplicitCastExpr(const ImplicitCastExpr *expr) {
+        switch(expr->getCastKind()) {
+        case clang::CK_BuiltinFnToFnPtr:
+        case clang::CK_FunctionToPointerDecay:
+            return Visit(expr->getSubExpr());
+        default:
+            return this->VisitCastExpr(expr);
+        }
+    }
+
+    // void VisitCallExpr(const CallExpr *expr) {
+    //     // TODO: this isn't going to work either because the types of arguments are going to matter
+    //     Visit(expr->getCallee());
+    // }
+
     void VisitDeclRefExpr(const DeclRefExpr *expr) {
-        cprint.printName(print, *expr->getDecl());
+        if (isa<ParmVarDecl>(expr->getDecl())) {
+            guard::ctor _{print, "Nlocal"};
+            guard::ctor __{print, "Nid", false};
+            cprint.printUnqualifiedName(print, *expr->getDecl());
+        } else {
+            cprint.printName(print, *expr->getDecl());
+        }
     }
 
     void VisitExpr(const Expr *expr) {
