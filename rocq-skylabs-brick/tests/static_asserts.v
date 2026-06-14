0420c0d588a8e92a95dff4c2b12ea7b78a59d3f4
Gregory Malecha <gregory@skylabs-ai.com>
2026-06-14 09:30:13 -0400

    Test case that definitions are ordered

--- rocq-skylabs-brick/tests/static_asserts.v
+++ rocq-skylabs-brick/tests/static_asserts.v
@@ -10,3 +10,16 @@ Goal module.(asserts) =
   [Build_StaticAssert "" (Ebool true);
    Build_StaticAssert "truth survives parsing" (Ebool true)]%pstring.
 Proof. reflexivity. Qed.
+
+cpp.prog ordered prog cpp:{{
+  static_assert(true, "a");
+  static_assert(true, "b");
+}}.
+
+cpp.prog permuted prog cpp:{{
+  static_assert(true, "b");
+  static_assert(true, "a");
+}}.
+
+Goal ordered = permuted.
+Proof. reflexivity. Qed.
