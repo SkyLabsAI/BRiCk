dea104f09dad896e54b37211b743fe4ef65c3614
Jan-Oliver Kaiser <janno@skylabs-ai.com>
2026-06-17 15:50:57 +0200

    Split examples

--- rocq-skylabs-brick/tests/float_support.v
+++ rocq-skylabs-brick/tests/float_support.v
@@ -91,85 +91,184 @@ Definition one_float : Expr := Efloat float_type.Ffloat f32_one.
 Definition one_double : Expr := Efloat float_type.Fdouble f64_one.
 Definition one_float128 : Expr := Efloat float_type.Ffloat128 f128_one.
 
-Example support_accepts_float_types :
-  check.type Tfloat16 = check.OK /\
-  check.type Tfloat = check.OK /\
-  check.type Tdouble = check.OK /\
+Example support_accepts_float16_type :
+  check.type Tfloat16 = check.OK.
+Proof. reflexivity. Qed.
+
+Example support_accepts_float_type :
+  check.type Tfloat = check.OK.
+Proof. reflexivity. Qed.
+
+Example support_accepts_double_type :
+  check.type Tdouble = check.OK.
+Proof. reflexivity. Qed.
+
+Example support_accepts_float128_type :
   check.type Tfloat128 = check.OK.
-Proof. repeat split; reflexivity. Qed.
+Proof. reflexivity. Qed.
 
 Example support_rejects_longdouble_type :
   check.type Tlongdouble = check.FAIL "unsupported floating width".
 Proof. reflexivity. Qed.
 
-Example support_accepts_float_literals :
-  check.expr one_float16 = check.OK /\
-  check.expr one_float = check.OK /\
-  check.expr one_double = check.OK /\
+Example support_accepts_float16_literal :
+  check.expr one_float16 = check.OK.
+Proof. reflexivity. Qed.
+
+Example support_accepts_float_literal :
+  check.expr one_float = check.OK.
+Proof. reflexivity. Qed.
+
+Example support_accepts_double_literal :
+  check.expr one_double = check.OK.
+Proof. reflexivity. Qed.
+
+Example support_accepts_float128_literal :
   check.expr one_float128 = check.OK.
-Proof. repeat split; reflexivity. Qed.
+Proof. reflexivity. Qed.
 
 Example support_rejects_longdouble_literal :
   check.expr (Efloat float_type.Flongdouble (float_value.zero float_type.Flongdouble)) =
     check.FAIL "unsupported floating width".
 Proof. reflexivity. Qed.
 
-Example support_accepts_float_casts :
-  check.expr (Ecast (Cfloat Tdouble) one_float) = check.OK /\
-  check.expr (Ecast (Cfloat Tfloat128) one_double) = check.OK /\
-  check.expr (Ecast (Cint2float Tfloat16) (Eint 0 Tint)) = check.OK /\
-  check.expr (Ecast (Cfloat2int Tint) one_float128) = check.OK /\
+Example support_accepts_float_to_double_cast :
+  check.expr (Ecast (Cfloat Tdouble) one_float) = check.OK.
+Proof. reflexivity. Qed.
+
+Example support_accepts_double_to_float128_cast :
+  check.expr (Ecast (Cfloat Tfloat128) one_double) = check.OK.
+Proof. reflexivity. Qed.
+
+Example support_accepts_int_to_float16_cast :
+  check.expr (Ecast (Cint2float Tfloat16) (Eint 0 Tint)) = check.OK.
+Proof. reflexivity. Qed.
+
+Example support_accepts_float128_to_int_cast :
+  check.expr (Ecast (Cfloat2int Tint) one_float128) = check.OK.
+Proof. reflexivity. Qed.
+
+Example support_accepts_float_to_bool_cast :
   check.expr (Ecast Cfloat2bool one_float) = check.OK.
-Proof. repeat split; reflexivity. Qed.
+Proof. reflexivity. Qed.
 
 Definition small_enum_name : name := "Small".
 
-Example support_accepts_char_enum_float_casts :
-  check.expr (Ecast (Cint2float Tfloat) (Echar 65 Tchar)) = check.OK /\
-  check.expr (Ecast (Cfloat2int Tchar) one_float) = check.OK /\
-  check.expr (Ecast (Cint2float Tfloat) (Eenum_const small_enum_name "A")) = check.OK /\
+Example support_accepts_char_to_float_cast :
+  check.expr (Ecast (Cint2float Tfloat) (Echar 65 Tchar)) = check.OK.
+Proof. reflexivity. Qed.
+
+Example support_accepts_float_to_char_cast :
+  check.expr (Ecast (Cfloat2int Tchar) one_float) = check.OK.
+Proof. reflexivity. Qed.
+
+Example support_accepts_enum_to_float_cast :
+  check.expr (Ecast (Cint2float Tfloat) (Eenum_const small_enum_name "A")) = check.OK.
+Proof. reflexivity. Qed.
+
+Example support_accepts_float_to_enum_cast :
   check.expr (Ecast (Cfloat2int (Tenum small_enum_name)) one_float) = check.OK.
-Proof. repeat split; reflexivity. Qed.
+Proof. reflexivity. Qed.
 
-Example support_accepts_float_ops :
-  check.expr (Eunop Uplus one_float Tfloat) = check.OK /\
-  check.expr (Eunop Uminus one_float Tfloat) = check.OK /\
-  check.expr (Eunop Unot one_float Tbool) = check.OK /\
-  check.expr (Ebinop Badd one_float one_double Tdouble) = check.OK /\
-  check.expr (Ebinop Bsub one_float one_float Tfloat) = check.OK /\
-  check.expr (Ebinop Bmul one_float one_float Tfloat) = check.OK /\
-  check.expr (Ebinop Bdiv one_float one_float Tfloat) = check.OK /\
+Example support_accepts_float_unary_plus :
+  check.expr (Eunop Uplus one_float Tfloat) = check.OK.
+Proof. reflexivity. Qed.
+
+Example support_accepts_float_unary_minus :
+  check.expr (Eunop Uminus one_float Tfloat) = check.OK.
+Proof. reflexivity. Qed.
+
+Example support_accepts_float_logical_not :
+  check.expr (Eunop Unot one_float Tbool) = check.OK.
+Proof. reflexivity. Qed.
+
+Example support_accepts_float_add :
+  check.expr (Ebinop Badd one_float one_double Tdouble) = check.OK.
+Proof. reflexivity. Qed.
+
+Example support_accepts_float_sub :
+  check.expr (Ebinop Bsub one_float one_float Tfloat) = check.OK.
+Proof. reflexivity. Qed.
+
+Example support_accepts_float_mul :
+  check.expr (Ebinop Bmul one_float one_float Tfloat) = check.OK.
+Proof. reflexivity. Qed.
+
+Example support_accepts_float_div :
+  check.expr (Ebinop Bdiv one_float one_float Tfloat) = check.OK.
+Proof. reflexivity. Qed.
+
+Example support_accepts_float_less_than :
   check.expr (Ebinop Blt one_float one_double Tbool) = check.OK.
-Proof. repeat split; reflexivity. Qed.
+Proof. reflexivity. Qed.
 
-Example support_rejects_unsupported_float_ops :
-  check.expr (Ebinop Bmod one_float one_float Tfloat) <> check.OK /\
-  check.expr (Ebinop Band one_float one_float Tfloat) <> check.OK /\
-  check.expr (Ebinop Bshl one_float (Eint 1 Tint) Tfloat) <> check.OK /\
-  check.expr (Ebinop Bcmp one_float one_float Tfloat) <> check.OK /\
-  check.expr (Ebinop Badd (Evar "p" (Tptr Tint)) one_float (Tptr Tint)) <> check.OK /\
-  check.expr (Epreinc (Evar "f" Tfloat) Tfloat) <> check.OK /\
+Example support_rejects_float_mod :
+  check.expr (Ebinop Bmod one_float one_float Tfloat) <> check.OK.
+Proof. vm_compute; discriminate. Qed.
+
+Example support_rejects_float_bitand :
+  check.expr (Ebinop Band one_float one_float Tfloat) <> check.OK.
+Proof. vm_compute; discriminate. Qed.
+
+Example support_rejects_float_shift_left :
+  check.expr (Ebinop Bshl one_float (Eint 1 Tint) Tfloat) <> check.OK.
+Proof. vm_compute; discriminate. Qed.
+
+Example support_rejects_float_three_way_compare :
+  check.expr (Ebinop Bcmp one_float one_float Tfloat) <> check.OK.
+Proof. vm_compute; discriminate. Qed.
+
+Example support_rejects_pointer_float_add :
+  check.expr (Ebinop Badd (Evar "p" (Tptr Tint)) one_float (Tptr Tint)) <> check.OK.
+Proof. vm_compute; discriminate. Qed.
+
+Example support_rejects_float_preinc :
+  check.expr (Epreinc (Evar "f" Tfloat) Tfloat) <> check.OK.
+Proof. vm_compute; discriminate. Qed.
+
+Example support_rejects_float_postdec :
   check.expr (Epostdec (Evar "f" Tfloat) Tfloat) <> check.OK.
-Proof. repeat split; vm_compute; discriminate. Qed.
+Proof. vm_compute; discriminate. Qed.
 
-Example usual_float_arith_examples tu :
-  usual_float_arith tu Tfloat16 Tfloat = Some Tfloat /\
-  usual_float_arith tu Tfloat Tdouble = Some Tdouble /\
+Example usual_float_arith_float16_float tu :
+  usual_float_arith tu Tfloat16 Tfloat = Some Tfloat.
+Proof. reflexivity. Qed.
+
+Example usual_float_arith_float_double tu :
+  usual_float_arith tu Tfloat Tdouble = Some Tdouble.
+Proof. reflexivity. Qed.
+
+Example usual_float_arith_float128_double tu :
   usual_float_arith tu Tfloat128 Tdouble = Some Tfloat128.
-Proof. repeat split; reflexivity. Qed.
+Proof. reflexivity. Qed.
 
-Example usual_float_arith_rejects_longdouble tu :
-  usual_float_arith tu Tlongdouble Tdouble = None /\
+Example usual_float_arith_rejects_longdouble_double tu :
+  usual_float_arith tu Tlongdouble Tdouble = None.
+Proof. reflexivity. Qed.
+
+Example usual_float_arith_rejects_double_longdouble tu :
   usual_float_arith tu Tdouble Tlongdouble = None.
-Proof. repeat split; reflexivity. Qed.
+Proof. reflexivity. Qed.
 
-Example convert_type_float_examples tu :
-  convert_type_op tu Badd Tfloat Tdouble = Some (Tdouble, Tdouble, Tdouble) /\
-  convert_type_op tu Badd Tfloat Tint = Some (Tfloat, Tfloat, Tfloat) /\
-  convert_type_op tu Bge Tint Tdouble = Some (Tdouble, Tdouble, Tbool) /\
-  convert_type_op tu Bmod Tfloat Tfloat = None /\
+Example convert_type_float_add_promotes_to_double tu :
+  convert_type_op tu Badd Tfloat Tdouble = Some (Tdouble, Tdouble, Tdouble).
+Proof. reflexivity. Qed.
+
+Example convert_type_float_int_add_converts_int tu :
+  convert_type_op tu Badd Tfloat Tint = Some (Tfloat, Tfloat, Tfloat).
+Proof. reflexivity. Qed.
+
+Example convert_type_int_double_ge_converts_int tu :
+  convert_type_op tu Bge Tint Tdouble = Some (Tdouble, Tdouble, Tbool).
+Proof. reflexivity. Qed.
+
+Example convert_type_rejects_float_mod tu :
+  convert_type_op tu Bmod Tfloat Tfloat = None.
+Proof. reflexivity. Qed.
+
+Example convert_type_rejects_pointer_float_add tu :
   convert_type_op tu Badd (Tptr Tint) Tfloat = None.
-Proof. repeat split; reflexivity. Qed.
+Proof. reflexivity. Qed.
 
 Example convert_includes_conv_float_for_float_to_int {σ : genv} tu v v' :
   conv_float tu Tfloat Tint v v' -> @convert σ tu Tfloat Tint v v'.
@@ -215,21 +314,45 @@ Example default_float128_zero :
   get_default Tfloat128 = Some (Vfloat float_type.Ffloat128 (float_value.zero float_type.Ffloat128)).
 Proof. reflexivity. Qed.
 
-Example float_bool_zero_cases :
-  is_true (Vfloat float_type.Ffloat (float_value.zero float_type.Ffloat)) = Some false /\
-  is_true (Vfloat float_type.Ffloat f32_neg_zero) = Some false /\
-  is_true (Vfloat float_type.Ffloat f32_one) = Some true /\
-  is_true (Vfloat float_type.Ffloat f32_inf) = Some true /\
-  is_true (Vfloat float_type.Ffloat f32_nan) = Some true.
-Proof. repeat split; vm_compute; reflexivity. Qed.
+Example float_zero_is_false :
+  is_true (Vfloat float_type.Ffloat (float_value.zero float_type.Ffloat)) = Some false.
+Proof. vm_compute; reflexivity. Qed.
 
-Example float16_and_float128_bool_cases :
-  is_true (Vfloat float_type.Ffloat16 f16_neg_zero) = Some false /\
-  is_true (Vfloat float_type.Ffloat16 f16_one) = Some true /\
-  is_true (Vfloat float_type.Ffloat128 f128_neg_zero) = Some false /\
-  is_true (Vfloat float_type.Ffloat128 f128_inf) = Some true /\
+Example float_negative_zero_is_false :
+  is_true (Vfloat float_type.Ffloat f32_neg_zero) = Some false.
+Proof. vm_compute; reflexivity. Qed.
+
+Example float_one_is_true :
+  is_true (Vfloat float_type.Ffloat f32_one) = Some true.
+Proof. vm_compute; reflexivity. Qed.
+
+Example float_infinity_is_true :
+  is_true (Vfloat float_type.Ffloat f32_inf) = Some true.
+Proof. vm_compute; reflexivity. Qed.
+
+Example float_nan_is_true :
+  is_true (Vfloat float_type.Ffloat f32_nan) = Some true.
+Proof. vm_compute; reflexivity. Qed.
+
+Example float16_negative_zero_is_false :
+  is_true (Vfloat float_type.Ffloat16 f16_neg_zero) = Some false.
+Proof. vm_compute; reflexivity. Qed.
+
+Example float16_one_is_true :
+  is_true (Vfloat float_type.Ffloat16 f16_one) = Some true.
+Proof. vm_compute; reflexivity. Qed.
+
+Example float128_negative_zero_is_false :
+  is_true (Vfloat float_type.Ffloat128 f128_neg_zero) = Some false.
+Proof. vm_compute; reflexivity. Qed.
+
+Example float128_infinity_is_true :
+  is_true (Vfloat float_type.Ffloat128 f128_inf) = Some true.
+Proof. vm_compute; reflexivity. Qed.
+
+Example float128_nan_is_true :
   is_true (Vfloat float_type.Ffloat128 f128_nan) = Some true.
-Proof. repeat split; vm_compute; reflexivity. Qed.
+Proof. vm_compute; reflexivity. Qed.
 
 Example eval_not_float_zero {σ : genv} tu :
   eval_unop tu Unot Tfloat Tbool
@@ -237,7 +360,7 @@ Example eval_not_float_zero {σ : genv} tu :
     (Vbool true).
 Proof.
   replace true with
-    (negb (float_value.is_true float_type.Ffloat (float_value.zero float_type.Ffloat))).
+    (negb (@float_value.is_true float_type.Ffloat (float_value.zero float_type.Ffloat))).
   - apply eval_not_float.
   - rewrite float_value.is_true_zero. reflexivity.
 Qed.
@@ -247,7 +370,7 @@ Example eval_not_float_nan {σ : genv} tu :
     (Vfloat float_type.Ffloat f32_nan)
     (Vbool false).
 Proof.
-  replace false with (negb (float_value.is_true float_type.Ffloat f32_nan)).
+  replace false with (negb (@float_value.is_true float_type.Ffloat f32_nan)).
   - apply eval_not_float.
   - vm_compute; reflexivity.
 Qed.
@@ -278,23 +401,41 @@ Example float_to_char_accepts_representable_char :
     (float_value.of_int float_type.Ffloat 65) = Some 65%N.
 Proof. vm_compute; reflexivity. Qed.
 
-Example raw_float_intro_examples {σ : genv} :
+Example raw_float16_intro {σ : genv} :
   raw_bytes_of_val σ Tfloat16 (Vfloat float_type.Ffloat16 f16_one)
-    (float_raw_bytes σ float_type.Ffloat16 f16_one) /\
+    (float_raw_bytes σ float_type.Ffloat16 f16_one).
+Proof. apply raw_bytes_of_val_float_intro. Qed.
+
+Example raw_float_intro {σ : genv} :
   raw_bytes_of_val σ Tfloat (Vfloat float_type.Ffloat f32_one)
-    (float_raw_bytes σ float_type.Ffloat f32_one) /\
+    (float_raw_bytes σ float_type.Ffloat f32_one).
+Proof. apply raw_bytes_of_val_float_intro. Qed.
+
+Example raw_double_intro {σ : genv} :
   raw_bytes_of_val σ Tdouble (Vfloat float_type.Fdouble f64_one)
-    (float_raw_bytes σ float_type.Fdouble f64_one) /\
+    (float_raw_bytes σ float_type.Fdouble f64_one).
+Proof. apply raw_bytes_of_val_float_intro. Qed.
+
+Example raw_float128_intro {σ : genv} :
   raw_bytes_of_val σ Tfloat128 (Vfloat float_type.Ffloat128 f128_one)
     (float_raw_bytes σ float_type.Ffloat128 f128_one).
-Proof. repeat split; apply raw_bytes_of_val_float_intro. Qed.
+Proof. apply raw_bytes_of_val_float_intro. Qed.
 
-Example float_bits_compatible_examples :
-  float_bits_compatible int_rank.Ishort float_type.Ffloat16 /\
-  float_bits_compatible int_rank.Iint float_type.Ffloat /\
-  float_bits_compatible int_rank.Ilong float_type.Fdouble /\
+Example float16_bits_compatible :
+  float_bits_compatible int_rank.Ishort float_type.Ffloat16.
+Proof. split; reflexivity. Qed.
+
+Example float32_bits_compatible :
+  float_bits_compatible int_rank.Iint float_type.Ffloat.
+Proof. split; reflexivity. Qed.
+
+Example double_bits_compatible :
+  float_bits_compatible int_rank.Ilong float_type.Fdouble.
+Proof. split; reflexivity. Qed.
+
+Example float128_bits_compatible :
   float_bits_compatible int_rank.I128 float_type.Ffloat128.
-Proof. repeat split; split; reflexivity. Qed.
+Proof. split; reflexivity. Qed.
 
 Example longdouble_raw_bits_not_binary128_compatible :
   ~ float_bits_compatible int_rank.I128 float_type.Flongdouble.
