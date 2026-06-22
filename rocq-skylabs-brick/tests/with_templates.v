abe510d64a4ac9a81f80173aaae473cae361216a
Gregory Malecha <gregory@skylabs-ai.com>
2026-06-22 12:18:35 -0400

    Re-work the options in the plugin

--- rocq-skylabs-brick/tests/with_templates.v
+++ rocq-skylabs-brick/tests/with_templates.v
@@ -2,8 +2,13 @@ Require Import skylabs.prelude.base.
 Require Import skylabs.lang.cpp.syntax.
 Require Import skylabs.lang.cpp.parser.plugin.cpp2v.
 
-#[with_templates]
 cpp.prog templated prog cpp:{{
+template <typename T> T id(T x) { return x; }
+int use() { return id<int>(3); }
+}}.
+
+#[no_templates]
+cpp.prog no_templated prog cpp:{{
 template <typename T> T id(T x) { return x; }
 int use() { return id<int>(3); }
 }}.
@@ -12,6 +17,12 @@ Definition is_Some {T} (a : option T) : Prop :=
   match a with
   | Some _ => True
   | None => False
+  end.
+
+Definition is_None {T} (a : option T) : Prop :=
+  match a with
+  | Some _ => False
+  | None => True
   end.
 
 Example has_template_symbols :
@@ -27,4 +38,15 @@ Proof. vm_compute. exact I. Qed.
 
 Example id_template_exists :
   is_Some (translation_unit.msymbols templated !! "id<$T>($T)"%cpp_name).
+Proof. vm_compute. exact I. Qed.
+
+Example no_template_symbols :
+  List.length (TM.elements no_templated.(translation_unit.msymbols)) = 0 := eq_refl.
+
+Example no_templated_id_int_exists :
+  is_Some (no_templated.(symbols) !! "id<int>(int)"%cpp_name).
+Proof. vm_compute. exact I. Qed.
+
+Example no_templated_id_template_absent :
+  is_None (translation_unit.msymbols no_templated !! "id<$T>($T)"%cpp_name).
 Proof. vm_compute. exact I. Qed.
