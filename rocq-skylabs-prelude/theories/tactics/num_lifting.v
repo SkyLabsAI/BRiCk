471c8fa3b67f56a2841b0f9bfaaebdb93d809729
Simon <simon@skylabs-ai.com>
2026-06-30 19:12:06 -0400

    Update rocq-skylabs-prelude/theories/tactics/num_lifting.v
    Co-authored-by: Gregory Malecha <gregory@skylabs-ai.com>

--- rocq-skylabs-prelude/theories/tactics/num_lifting.v
+++ rocq-skylabs-prelude/theories/tactics/num_lifting.v
@@ -1,5 +1,5 @@
 (*
- * Copyright (C) 2022 BlueRock Security, Inc.
+ * Copyright (C) 2026 SkyLabs AI, Inc.
  *
  * This software is distributed under the terms of the BedRock Open-Source License.
  * See the LICENSE-BedRock file in the repository root for details.
