2ee9ef6eff2f2258f0ec1a7b127d50b17a6c4b6c
Paolo G. Giarrusso <paolo@skylabs-ai.com>
2026-07-13 15:37:13 +0200

    WIP (from agent?)

--- rocq-skylabs-brick/tests/typed_decltype_checks.v
+++ rocq-skylabs-brick/tests/typed_decltype_checks.v
@@ -1,22 +1,6 @@
 Require Import skylabs.lang.cpp.syntax.typed.
 Require Import skylabs.lang.cpp.cpp.
-Require Import skylabs.lang.cpp.parser.plugin.cpp2v.
-
-#[duplicates(error)]
-cpp.prog source flags "-std=c++20" prog cpp:{{
-  struct C {
-    int f;
-    C(int);
-  };
-
-  struct D {
-    D();
-  };
-
-  void not_ctor(C*, int) {}
-
-  int global_int;
-}}.
+Require Import skylabs.lang.cpp.parser.
 
 Definition C_name : name := Nglobal (Nid "C").
 Definition D_name : name := Nglobal (Nid "D").
@@ -27,6 +11,37 @@ Definition global_int_name : name := Nglobal (Nid "global_int").
 Definition C_field_f : name := Nscoped C_name (Nid "f").
 Definition C_field_missing : name := Nscoped C_name (Nid "missing").
 Definition missing_global : name := Nglobal (Nid "missing").
+
+Definition C_struct : Struct :=
+  Build_Struct nil
+    (mkMember (Nid "f") Tint false None {| li_offset := 0 |} :: nil)
+    nil nil (Nscoped C_name Ndtor) true None POD 4 4.
+
+Definition D_struct : Struct :=
+  Build_Struct nil nil nil nil (Nscoped D_name Ndtor) true None POD 1 1.
+
+Definition C_ctor : Ctor :=
+  Build_Ctor C_name (("x"%pstring, Tint) :: nil) CC_C Ar_Definite
+    exception_spec.Unknown None.
+
+Definition D_ctor : Ctor :=
+  Build_Ctor D_name nil CC_C Ar_Definite exception_spec.Unknown None.
+
+Definition not_ctor : Func :=
+  Build_Func Tvoid (("self"%pstring, Tptr (Tnamed C_name)) :: ("x"%pstring, Tint) :: nil)
+    CC_C Ar_Definite exception_spec.Unknown None.
+
+Definition source : translation_unit :=
+  Eval vm_compute in
+    fst (parser.translation_unit.list_decls
+      (parser.Dstruct C_name (Some C_struct) ::
+       parser.Dstruct D_name (Some D_struct) ::
+       parser.Dconstructor C_ctor_int C_ctor ::
+       parser.Dconstructor (Nscoped D_name (Nctor nil)) D_ctor ::
+       parser.Dfunction not_ctor_name not_ctor ::
+       parser.Dvariable global_int_name Tint global_init.NoInit ::
+       nil)
+      abi.abi_default).
 
 Goal exists ov, source.(symbols) !! C_ctor_int = Some ov.
 Proof. vm_compute. eauto. Qed.
