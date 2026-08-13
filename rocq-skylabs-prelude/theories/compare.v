9a6bcb47ca2506708e391417ab01e58de8e82e50
Paolo G. Giarrusso <paolo@skylabs-ai.com>
2026-08-13 17:04:47 +0200

    prelude.compare: more compact theory

--- rocq-skylabs-prelude/theories/compare.v
+++ rocq-skylabs-prelude/theories/compare.v
@@ -71,48 +71,50 @@ Module LeibnizComparison.
    *)
   Class C {T} (cmp : T -> T -> comparison) : Prop :=
     cmp_eq : forall a b, cmp a b = Eq -> a = b.
-  Arguments cmp_eq {_} _ {_} _ _.
+  #[global] Arguments cmp_eq {_} _ {_} _ _.
 
-  Lemma comparison_refl {A cmp} {C : Comparison cmp} :
-    forall a : A, cmp a a = Eq.
-  Proof.
-    intros.
-    generalize (compare_antisym a a).
-    destruct (cmp a a); simpl; eauto; inversion 1.
-  Qed.
+  Section with_A.
+    Context {A : Type}.
+    Implicit Type (a b : A).
 
-  Lemma comparison_not_eq {A cmp} {C : Comparison cmp} :
-    forall a b : A, cmp a b <> Eq -> a <> b.
+    Section with_Comparison.
+      Context `{Comp : !Comparison (A := A) cmp}.
+      #[local] Set Default Proof Using "Comp".
+
+      Lemma comparison_refl {a} : cmp a a = Eq.
+      Proof.
+        have := Refine (compare_antisym a a).
+        by case: (cmp a a).
+      Qed.
+
+      Lemma comparison_not_eq {a b} : cmp a b <> Eq -> a <> b.
+      Proof. intros NE ->. by rewrite comparison_refl in NE. Qed.
+
+      (* TODO: make instance? *)
+      #[program] Definition from_comparison {LC : C cmp} : EqDecision A := fun l r =>
+        match cmp l r as C return cmp l r = C -> _ with
+        | Eq => fun pf => left (LC _ _ pf)
+        | Lt => fun pf => right _
+        | Gt => fun pf => right _
+        end eq_refl.
+      Next Obligation. intros ** ->. by rewrite -> comparison_refl in *. Qed.
+      Next Obligation. intros ** ->. by rewrite -> comparison_refl in *. Qed.
+    End with_Comparison.
+  End with_A.
+
+  Lemma PrimInt63_int_compare_eq (x y : PrimInt63.int) :
+    PrimInt63.compare x y = Eq ->
+    PrimInt63.eqb x y = true.
   Proof.
-    intros; intro. subst. apply H. apply comparison_refl.
-  Qed.
-  Lemma comparison_not_eq_Lt {A cmp} {C : Comparison cmp} :
-    forall {a b : A}, cmp a b = Lt -> a <> b.
-  Proof.
-    intros; intro. subst. eapply (comparison_not_eq b b); congruence.
-  Qed.
-  Lemma comparison_not_eq_Gt {A cmp} {C : Comparison cmp} :
-    forall {a b : A}, cmp a b = Gt -> a <> b.
-  Proof.
-    intros; intro. subst. eapply (comparison_not_eq b b); congruence.
+    rewrite Uint63Axioms.compare_def_spec /Uint63Axioms.compare_def.
+    repeat case_match; congruence.
   Qed.
 
   Definition by_prim_tag {T} (f : T -> PrimInt63.int) {Hinj : Inj eq eq f}
     : C (fun a b => PrimInt63.compare (f a) (f b)).
   Proof.
-    red; move=> ? ? H. apply Hinj.
-    rewrite Uint63Axioms.compare_def_spec in H.
-    compute in H.
-    repeat case_match; try congruence.
-    by apply Uint63.eqb_spec.
+    move=> a b E. apply (inj f), Uint63.eqb_spec, PrimInt63_int_compare_eq, E.
   Qed.
-
-  Definition from_comparison {A cmp} {Cmp : Comparison cmp} {LC : @C A cmp} : EqDecision A :=
-    fun l r => match cmp l r as C return cmp l r = C -> _ with
-            | Eq => fun pf => left (LC _ _ pf)
-            | Lt => fun pf => right (comparison_not_eq_Lt pf)
-            | Gt => fun pf => right (comparison_not_eq_Gt pf)
-            end eq_refl.
 
 End LeibnizComparison.
 Notation LeibnizComparison := LeibnizComparison.C.
